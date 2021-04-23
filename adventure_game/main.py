6fe9a11b27a430b557f1ed82689b5978177074d8
arcadecoder <arcade.kwaku@gmail.com>
2021-04-23 12:32:13 +0100

    add adventure game file

---
+++ adventure_game/main.py
@@ -0,0 +1,22 @@
+import time 
+import random 
+
+
+random_creatures = ["gorgon", "vampire", "gruffalo", "zombie", "minatour"]
+
+print('''
+You find yourself in a dark cave 
+You only have a lollipop in your hand
+''')
+choice = input("Do you to exit the cave? Type Y/N \n")
+
+if choice == "Y":
+    print("You find yourself in a field of flowers")
+else: 
+    print('''Stumbling around the cave, you hear an ominous noise coming from deep in the darness \n ''')
+    creature  = random.choice(random_creatures)
+    print(f"Oh no! You encounter a {creature}")
+
+
+#TO DO: Wrap the introduction in  a function - Ellen
+#TO DO: create a second part of the introduction that occurs when you type Yes - Sophie
