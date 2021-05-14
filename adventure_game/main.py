27286eb038aaba71763285055597720b6604d815
arcadecoder <arcade.kwaku@gmail.com>
2021-05-14 09:59:36 +0100

    adds new homework

--- adventure_game/main.py
+++ adventure_game/main.py
@@ -17,6 +17,22 @@ def print_pause(string):
 def greet_user(username):
     print_pause("Hello, " + username + "!")
 
+def item_pickup():
+    print_pause("You see a faint glimmer in the sunshine")
+    print_pause("You notice that it is in fact a sword")
+    choice = input("Do you want to pick up the sword? Y/N ")
+    if choice == "Y":
+        items.append("sword")
+        inventory = ', '.join(items)
+        print_pause(f"You're inventory now contains {len(items)} items. You have: \n {inventory}")
+    else: 
+        print_pause("Are you sure?")
+
+
+def witch_house():
+    #TO DO: Joe add more exploration to the story and then add this function to the first_choice()
+    return 
+
 
 def first_choice():
     '''
@@ -26,10 +42,14 @@ def first_choice():
     choice = input("Do you to exit the cave? Type Y/N \n")
     if choice == "Y":
         print_pause("You find yourself in a field of flowers")
+        item_pickup()
+        #TO DO: Add more exploration to the story. - Joe
     else: 
         print_pause('''Stumbling around the cave, you hear an ominous noise coming from deep in the darness \n ''')
         creature  = random.choice(random_creatures)
         print_pause(f"Oh no! You encounter a {creature}")
+        #TO DO: new function that gives a choice to fight the monster or not - Ellen 
+        #TO DO: function should first show XP points of the monster & inventory before presenting choice. - Ellen 
 
 
 def intro(): 
@@ -38,8 +58,6 @@ def intro():
     greet_user('Ellen')
     greet_user('Sophie')
     greet_user('Joe')
-
-    # TO DO: Ellen - figure out how to print items as a string without []
     string_items = ', '.join(items)
     print_pause("You find yourself in a dark cave and you only have a " + string_items + " in your hand")
     first_choice()
