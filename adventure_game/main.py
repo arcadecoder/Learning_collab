b07b400fa3ff732afd2bf94fe4c5c4a9aa636222
Ellen-548 <ellengoves@gmail.com>
2021-05-14 16:55:29 +0100

    Hi A, here is my omework with the new creature function. E

--- adventure_game/main.py
+++ adventure_game/main.py
@@ -24,9 +24,19 @@ def item_pickup():
     if choice == "Y":
         items.append("sword")
         inventory = ', '.join(items)
-        print_pause(f"You're inventory now contains {len(items)} items. You have: \n {inventory}")
+        print_pause(f"Your inventory now contains {len(items)} items. You have: \n {inventory}")
     else: 
         print_pause("Are you sure?")
+
+def fight_monster(creature):
+    print_pause("You currently have 5XP Points.")
+    print_pause(f"The {creature} has 10XP Points")
+    choice = input(f"Do you want to fight the {creature}? Y/N")
+    if choice == "Y":
+        print_pause(f"You try your best, but the {creature} knocks you to the ground")
+        print_pause("You now have 0XP Points")
+    else: 
+        print_pause(f"You run away from the {creature} crying")
 
 
 def witch_house():
@@ -48,12 +58,13 @@ def first_choice():
         print_pause('''Stumbling around the cave, you hear an ominous noise coming from deep in the darness \n ''')
         creature  = random.choice(random_creatures)
         print_pause(f"Oh no! You encounter a {creature}")
+        fight_monster(creature)
+
         #TO DO: new function that gives a choice to fight the monster or not - Ellen 
         #TO DO: function should first show XP points of the monster & inventory before presenting choice. - Ellen 
 
 
 def intro(): 
-    
     greet_user('Arcade') #calling the function
     greet_user('Ellen')
     greet_user('Sophie')
@@ -61,6 +72,9 @@ def intro():
     string_items = ', '.join(items)
     print_pause("You find yourself in a dark cave and you only have a " + string_items + " in your hand")
     first_choice()
+
+
+
 
 
 intro()
