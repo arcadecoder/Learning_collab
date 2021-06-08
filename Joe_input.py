8f56119414ddecc838310cd6514686478c7f385b
Watjoey <watjoey238@gmail.com>
2021-06-08 09:37:13 +0100

    test

---
+++ Joe_input.py
@@ -0,0 +1,7 @@
+# joe's attempt at making a valid input option
+
+Y = ['YES','yes','Y','y','Yeah','yeah']
+N = ['NO', 'no', 'N', 'n', 'Nah', 'nah']
+
+# would this require replacing all the 'Y' and 'N' in the code and adding
+print(Y)
