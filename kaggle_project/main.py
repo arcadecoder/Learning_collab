ce800738cfbeb4846b0b0d1743b44b3fa6857f2c
arcadecoder <arcade.kwaku@gmail.com>
2021-06-10 10:12:28 +0100

    adds graph function

--- kaggle_project/main.py
+++ kaggle_project/main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy
+import matplotlib.pyplot as plt 
 
 ecg_df = pd.read_csv("ecg.csv")
-#print(ecg_df.head())
-
 
 def convert_dataframe(ecg_df):
     ''' function which 
     splits a colon delimited csv into 
-    properly split dataframe'''
+    properly split dataframe
+    '''
 
     data_list = []
 
@@ -19,9 +19,27 @@ def convert_dataframe(ecg_df):
             'heartrate': data[1]
         }
         data_list.append(data_dict)
-
-    print(data_list)
-
+    # local function - only exists within the
     df = pd.DataFrame(data_list)
 
     print(df.head())
+    return df
+
+
+
+df = pd.read_csv("ecg.csv", sep=';', index_col="ms")
+
+
+def plot_ecg_heart_rate(df):
+    '''
+    Takes data from ecg df and create a graph
+    '''
+    fig = plt.figure(figsize=(20,7))
+    plt.plot(df[0:5000].index, df[0:5000].heartrate, linewidth=1)
+    plt.xlabel("Time (ms)", fontsize=20)
+    plt.ylabel("Amplitude", fontsize=20)
+    plt.show()
+    fig.savefig("test.pdf")
+    return 
+
+plot_ecg_heart_rate(df)
